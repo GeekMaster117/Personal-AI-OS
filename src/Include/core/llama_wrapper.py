7016b022addd489ecf78f8d8b8f5ab3f94aa8a56
GeekMaster117 <saivenkat0306@gmail.com>
2025-07-21 21:44:30 +0530

    Added logic to select best gpu among multiple gpus

--- src/Include/core/llama_wrapper.py
+++ src/Include/core/llama_wrapper.py
@@ -8,33 +8,57 @@ import settings
 
 class LlamaCPP:
     def __init__(self):
-        arch = self._get_nvidia_gpu_arch()
-        cuda_library = self._get_cuda_library(arch)
+        self.best_gpu_info = self._get_gpu_info()
+
+        cuda_library = self._get_cuda_library(self.best_gpu_info['arch'])
         if cuda_library:
             ctypes.CDLL(cuda_library)
-        print('Running tests...')
-        print('GPU acceleration successfully works.')
-        
+
         llm = Llama(
-            model_path="models/phi-2.Q4_K_M.gguf",  # <-- Update path to your model
-            n_ctx=512,
-            n_threads=6,  # Or however many threads your CPU can handle
-            n_gpu_layers=30
+            model_path = settings.model_dir,
+            main_gpu = self.best_gpu_info['idx'],
+            n_ctx = 2048,
+            n_threads = os.cpu_count(),
+            n_gpu_layers = 30
         )
         output = llm("Q: What is the capital of India?\nA:", max_tokens=32, stop=["\n"])
         print(output)
 
-    def _get_nvidia_gpu_arch(self) -> int | None:
+    def _get_gpu_info(self) -> list[int] | None:
         try:
-            if cuda.Device.count() < 1:
+            best_gpu_info = dict()
+            for idx in range(cuda.Device.count()):
+                device = cuda.Device(idx)
+                context = device.make_context()
+                free_mem, total_mem = cuda.mem_get_info()
+                context.pop()
+
+                if best_gpu_info:
+                    if (best_gpu_info['free_mem'] < free_mem) or (best_gpu_info['free_mem'] == free_mem and best_gpu_info['total_mem'] < total_mem):
+                        best_gpu_info = {
+                            'idx': idx,
+                            'free_mem': free_mem,
+                            'total_mem': total_mem
+                        }
+                else:
+                    best_gpu_info = {
+                        'idx': idx,
+                        'free_mem': free_mem,
+                        'total_mem': total_mem
+                    }
+
+            if not best_gpu_info:
                 return None
             
-            device = cuda.Device(0).compute_capability()
-            return (device[0] * 10) + device[1]
+            compute_capability = cuda.Device(best_gpu_info['idx']).compute_capability()
+            arch = (compute_capability[0] * 10) + compute_capability[1]
+            best_gpu_info['arch'] = arch
+
+            return best_gpu_info
         except:
             return None
         
-    def _get_cuda_library(self, arch) -> str | None:
+    def _get_cuda_library(self, arch: int) -> str | None:
         if not arch:
             print("Unable to detect supported GPU, skipping GPU acceleration")
             return
