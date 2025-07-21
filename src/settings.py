ac280ebe2b88f9e5dc01bb55011d1c163d26c931
GeekMaster117 <saivenkat0306@gmail.com>
2025-07-21 14:18:08 +0530

    Moved it to root dir

--- src/settings.py
+++ src/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 tick: timedelta = timedelta(seconds=30)
 time_threshold: timedelta = timedelta(minutes=3)
 
-metadata_dir: Path = Path(__file__).resolve().parent.parent.parent / "metadata"
+metadata_dir: Path = Path(__file__).resolve().parent / "metadata"
 
 model_name: str = "phi2"
 experimental_model_name: str = "phi3:mini"
