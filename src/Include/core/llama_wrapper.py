2a178441ed1bf99924e49b879c1832cbb0085b87
GeekMaster117 <saivenkat0306@gmail.com>
2025-07-23 22:04:24 +0530

    Added data processing

--- src/Include/core/llama_wrapper.py
+++ src/Include/core/llama_wrapper.py
@@ -1,8 +1,16 @@
 import pycuda.autoinit
 import pycuda.driver as cuda
 import os
+import threading
+import sys
+import time
+import itertools
+import textwrap
 import ctypes
 from llama_cpp import Llama
+
+from Include.core.suggestion_engine import SuggestionEngine
+from Include.core.metadatadb import MetadataDB
 
 import settings
 
@@ -22,10 +30,46 @@ class LlamaCPP:
                 n_ctx = settings.model_window_size,
                 n_threads = os.cpu_count(),
                 n_gpu_layers = self.best_gpu_info['gpu_layers'],
-                n_batch = self.best_gpu_info['batch_size']
+                n_batch = self.best_gpu_info['batch_size'],
+                verbose = False
             )
-            output = llm("Q: What is the capital of India?\nA:", max_tokens=32, stop=["\n"])
-            print(output)
+
+            db_handler = MetadataDB(settings.metadata_dir)
+            suggestion_engine = SuggestionEngine(db_handler)
+
+            messages = [
+                {"role": "system", "content": self._get_system_prompt()},
+                {"role": "assistant", "content": self._get_assistant_prompt(suggestion_engine.processed_logs.get())},
+                {"role": "user", "content": ""}
+            ]
+
+            while True:
+                user_input = input("You: ")
+                if user_input.lower() in ['exit', 'quit', 'stop']:
+                    print("Exiting conversation.")
+                    break
+
+                messages[-1]["content"] = user_input
+
+                spinner_flag = {'running': True}
+                spinner_thread = threading.Thread(
+                    target = self._loading_spinner,
+                    args = ("Thinking", spinner_flag,)
+                )
+                spinner_thread.start()
+
+                first_chunk = True
+                for chunk in llm.create_chat_completion(messages=messages, stream=True):
+                    if first_chunk:
+                        spinner_flag['running'] = False
+                        spinner_thread.join()
+                        first_chunk = False
+                        print("LLM: ", end='', flush=True)
+
+                    delta = chunk["choices"][0]["delta"]
+                    content = delta.get("content", "")
+                    print(content, end='', flush=True)
+                print("\n")
         else:
             print("Skipping GPU acceleration...")
 
@@ -171,6 +215,90 @@ class LlamaCPP:
 
             supported_arch.add(int(arch))
 
-        return supported_arch            
+        return supported_arch
+    
+    def _get_system_prompt(self) -> str:
+        return textwrap.dedent(f"""
+            You are a Personal AI Meta Operating System that gives user suggestions based on their app data.
+
+            You speak warmly and professionally, like a calm coach clear, human, and judgment-free.
+
+            You can generate suggestions in four categories:
+            1. Routine: Recognise hidden or visible patterns in the app data, and provide insights on it.
+            2. Personal: Give suggestions to improve personal life based on app data.
+            3. Professional: Give suggestions to improve professional life based on app data.
+            4. Productivity: Give suggestions to improve productivity based on app data.
+
+            You will be given app data in the following format:
+            Date created: 2025-07-23T19:22:45.123456 in the format of YYYY-MM-DDTHH:MM:SS.ffffff
+            Top {settings.data_limit} Apps and their Top {settings.data_limit} Titles:
+
+            1. App name1:
+            - Total Focus Duration: Total time spent actively on app. Example - 1.5 hours
+            - Total Duration: Total time spent on app actively and passively. Example - 2 hours
+            - Hourly Focus Duration: [hour1: time spent actively in hour1, hour2: time spent actively in hour 2, ........, hourN: time spent actively in hourN]. Example - [2PM: 30.7 minutes, 4PM: 30 seconds]
+
+            - 1.1 Title name1:
+            -- Total Focus Duration: Total time spent actively on title. Example - 30.6 minutes
+            -- Total Duration: Total time spent on title actively and passively. Example - 49.3 minutes
+            -- Hourly Focus Duration: [hour1: time spent actively in hour1, hour2: time spent actively in hour 2, ........, hourN: time spent actively in hourM]. Example - [2PM: 20.4 minutes]
+
+            Between 1.1 and 1.{settings.data_limit} there will be similar data on titles, analyze them similarly
+
+            - 1.{settings.data_limit} Title name{settings.data_limit}:
+            -- Total Focus Duration: Total time spent actively on title. Example - 10 seconds
+            -- Total Duration: Total time spent on title actively and passively. Example - 1.5 minutes
+            -- Hourly Focus Duration: [hour1: time spent actively in hour1, hour2: time spent actively in hour 2, ........, hourN: time spent actively in hourM]. Example - [4PM: 10 seconds]
+
+            Between 1 and {settings.data_limit} there will be similar data on apps, analyze them similarly
+
+            {settings.data_limit}. App name{settings.data_limit}:
+            - Total Focus Duration: Total time spent actively on app. Example - 4.3 hours
+            - Total Duration: Total time spent on app actively and passively. Example - 6.7 hours
+            - Hourly Focus Duration: [hour1: time spent actively in hour1, hour2: time spent actively in hour 2, ........, hourN: time spent actively in hourN]. Example - [11AM: 2.9 hours, 9AM: 1 hour, 12PM: 20.3 minutes]
+
+            - {settings.data_limit}.1 Title name1:
+            -- Total Focus Duration: Total time spent actively on title. Example - 40 minutes
+            -- Total Duration: Total time spent on title actively and passively. Example - 1.1 hours
+            -- Hourly Focus Duration: [hour1: time spent actively in hour1, hour2: time spent actively in hour 2, ........, hourN: time spent actively in hourM]. Example - [9PM: 40 minutes]
+
+            Between {settings.data_limit}.1 and {settings.data_limit}.{settings.data_limit} there will be similar data on titles, analyze them similarly
+
+            - {settings.data_limit}.{settings.data_limit} Title name{settings.data_limit}:
+            -- Total Focus Duration: Total time spent actively on title. Example - 2.6 hours
+            -- Total Duration: Total time spent on title actively and passively. Example - 5.4 hours
+            -- Hourly Focus Duration: [hour1: time spent actively in hour1, hour2: time spent actively in hour 2, ........, hourN: time spent actively in hourM]. Example - [9AM: 14.6 minutes, 11AM: 2.9 hours]
+
+
+            A title is a specific window or file, e.g., "YouTube - Firefox" or "main.py - VSCode".
+            An app is a general application (e.g., Firefox, VSCode).
+            Hourly Focus Duration are grouped by hour. Each hour contains how much time spent actively in that hour (Hour is represented in 12-hour format)
+
+            Instructions:
+            - NEVER assume or hallucinate any missing data.
+            - ONLY MENTION the data you see in the app data, if the data you want to mention is not in the app data, do not mention it.
+            - If there is not enough data to make a suggestion, say exactly: "Not enough data to make suggestions."
+            - You may provide 0 to 2 suggestions from any of the categories, but only if justified by data.
+            - Each suggestion must be 1 to 2 concise sentences, and must be only 1 paragraph.
+            - Each sentence must be 10 to 20 words long.
+            - If you are unsure whether a suggestion is supported, do not provide one for that category.
+
+            You are not just analyzing — you are mentoring gently, like a productivity coach fused into an OS.
+        """)
+    
+    def _get_assistant_prompt(self, data) -> str:
+        return textwrap.dedent(f"""
+            Use this data to provide suggestions:
+            {data}
+        """)
+
+    def _loading_spinner(self, loading_message, flag):
+        spinner = itertools.cycle(['|', '/', '-', '\\'])
+        while flag['running']:
+            sys.stdout.write(f"\r{loading_message}... {next(spinner)}")
+            sys.stdout.flush()
+            time.sleep(0.1)
+        sys.stdout.write('\r \r')
+        sys.stdout.flush()
 
 test = LlamaCPP()
