6aa56d3277710a3eadc9fb8b14b8d6cd742b8597
GeekMaster117 <saivenkat0306@gmail.com>
2025-07-23 13:41:25 +0530

    Added model attributes

--- src/settings.py
+++ src/settings.py
@@ -8,6 +8,10 @@ metadata_dir: str = "metadata"
 cuda_dir: str = "bin"
 
 model_dir: str = "models/phi-2.Q4_K_M.gguf"
+total_model_layers = 32
+model_window_size = 2048
+forward_pass_duration = 0.1
+
 experimental_model_name: str = "phi3:mini"
 
 max_logs: int = 7
