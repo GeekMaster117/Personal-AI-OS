42dcc741a4cabb74e5ed6141ebf0d94dc6d7948e
GeekMaster117 <saivenkat0306@gmail.com>
2025-08-10 22:58:03 +0530

    Added GPU benchmark

--- benchmark.py
+++ benchmark.py
@@ -7,30 +7,59 @@ import settings
 class Benchmark:
     test_prompt = "Hello " * 10
     max_tokens = 50
+    batch_sizes = [4, 6, 8, 16, 32, 64]
 
-    def run_cpu_benchmark() -> int:
+    def get_cpu_optimal_batchsize() -> int:
         print("Running CPU benchmark...", flush=True)
 
         def test_batchsize(batch_size: int) -> int:
-            llama = LlamaCPP(gpu_max_batch_size = 0, cpu_max_batch_size = batch_size, gpu_acceleration = False, use_cache = False)
+            llama = LlamaCPP(gpu_optimal_batchsize = 0, cpu_optimal_batchsize = batch_size, gpu_acceleration = False, cache = False)
             throughput = llama.run_inference(Benchmark.test_prompt, Benchmark.max_tokens)
             del llama
 
             return throughput
 
-        two_powers = [i for i in range(2, 7)]
-
         best_batchsize = 2
         best_throughput = 0
-        for power in two_powers:
-            batch_size = 2 ** power
+        for batch_size in Benchmark.batch_sizes:
             print("\nTesting CPU throughput with batch size:", batch_size)
-
-            throughput = test_batchsize(2 ** power)
-            print("Throughput:", throughput)
+            throughput = test_batchsize(batch_size)
 
             if throughput > best_throughput:
                 best_batchsize = batch_size
                 best_throughput = throughput
+            else:
+                break
 
         return best_batchsize
+    
+    def get_gpu_optimal_batchsize() -> int:
+        print("Running GPU benchmark...", flush=True)
+
+        if not LlamaCPP.supports_gpu_acceleration():
+            print("Your system does not support GPU acceleration")
+            return 0
+
+        def test_batchsize(batch_size: int) -> int:
+            llama = LlamaCPP(gpu_optimal_batchsize = batch_size, cpu_optimal_batchsize = 0, gpu_acceleration = True, cache = False)
+            throughput = llama.run_inference(Benchmark.test_prompt, Benchmark.max_tokens)
+            del llama
+
+            return throughput
+        
+        best_batchsize = 2
+        best_throughput = 0
+        for batch_size in Benchmark.batch_sizes:
+            print("\nTesting GPU throughput with batch size:", batch_size)
+            throughput = test_batchsize(batch_size)
+
+            if throughput > best_throughput:
+                best_batchsize = batch_size
+                best_throughput = throughput
+            else:
+                break
+
+        return best_batchsize
+    
+print(Benchmark.get_cpu_optimal_batchsize())
+print(Benchmark.get_gpu_optimal_batchsize())
