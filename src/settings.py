a18e5e7de7b5097a7df76d769d925d487ae204d1
GeekMaster117 <saivenkat0306@gmail.com>
2025-08-16 21:21:44 +0530

    Added sql and schema dir to settings

--- src/settings.py
+++ src/settings.py
@@ -5,9 +5,11 @@ tick: timedelta = timedelta(seconds=30)
 time_threshold: timedelta = timedelta(minutes=3)
 
 usagedata_dir: str = "data"
+sql_dir: str = "sql"
+schema_dir: str = sql_dir + "/schema.sql"
+
 cuda_dir: str = "bin"
 sys_cache_dir: str = cuda_dir + "/sys_cache.bin"
-
 model_dir: str = "models/Phi-3-mini-4k-instruct-q4.gguf"
 total_model_layers: int = 32
 model_window_size: int = 4096
