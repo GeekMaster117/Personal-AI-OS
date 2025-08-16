6c0eb2084c27cb5bf68e8fa8fa80401da841c2b4
GeekMaster117 <saivenkat0306@gmail.com>
2025-08-16 22:33:10 +0530

    Made get conn method private replaces query method with fetchall and fetchone

--- src/Include/wrapper/sqlite_wrapper.py
+++ src/Include/wrapper/sqlite_wrapper.py
@@ -11,7 +11,7 @@ class SQLiteWrapper:
         self._initialize_db()
 
     @contextmanager
-    def get_conn(self):
+    def _get_conn(self):
         with self._lock:
             conn = sqlite3.connect(self.db_path, timeout=30, isolation_level=None)
             conn.row_factory = sqlite3.Row
@@ -21,20 +21,24 @@ class SQLiteWrapper:
                 conn.close()
 
     def _initialize_db(self):
-        with self.get_conn() as conn:
+        with self._get_conn() as conn:
             conn.execute("PRAGMA journal_mode=WAL;")
             conn.execute("PRAGMA synchronous=NORMAL;")
             conn.execute("PRAGMA foreign_keys=ON;")
 
     def execute_file(self, file_path: str):
-        with self.get_conn() as conn:
+        with self._get_conn() as conn:
             with open(file_path, "r") as f:
                 conn.executescript(f.read())
 
-    def query(self, query: str):
-        with self.get_conn() as conn:
-            return conn.execute(query).fetchall()
+    def execute(self, query: str, params: tuple = ()) -> None:
+        with self._get_conn() as conn:
+            conn.execute(query, params)
 
-    def execute(self, query: str) -> None:
-        with self.get_conn() as conn:
-            conn.execute(query)
+    def fetchall(self, query: str, params: tuple = ()) -> list[sqlite3.Row]:
+        with self._get_conn() as conn:
+            return conn.execute(query, params).fetchall()
+
+    def fetchone(self, query: str, params: tuple = ()) -> sqlite3.Row | None:
+        with self._get_conn() as conn:
+            return conn.execute(query, params).fetchone()
