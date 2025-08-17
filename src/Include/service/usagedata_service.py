7acf8e15d7e3f563f13a664a48dbf5847726fd26
GeekMaster117 <saivenkat0306@gmail.com>
2025-08-17 16:34:03 +0530

    Added update_latest_day_log Made the code clean and scalable

--- src/Include/service/usagedata_service.py
+++ src/Include/service/usagedata_service.py
@@ -3,18 +3,33 @@ from Include.wrapper.sqlite_wrapper import SQLiteWrapper
 import settings
 
 class UsagedataService:
+    _day_log_columns: tuple[str] = (
+        "time_anchor",
+        "monotonic_start",
+        "monotonic_last_updated",
+        "total_downtime_duration",
+        "total_anomalies"
+    )
+
     def __init__(self, usagedata_dir: str):
         self._db = SQLiteWrapper(usagedata_dir)
 
     def create_if_not_exists_schema(self) -> None:
         self._db.execute_file(settings.schema_dir)
 
-    def add_day_log(self, time_anchor: str, monotonic_start: float, monotonic_last_updated: float) -> None:
+    def add_day_log(self, time_anchor: str, monotonic_anchor: float) -> None:
         query = "INSERT INTO day_log (time_anchor, monotonic_start, monotonic_last_updated) VALUES (?, ?, ?)"
-        self._db.execute(query, (time_anchor, monotonic_start, monotonic_last_updated))
+        self._db.execute(query, (time_anchor, monotonic_anchor, monotonic_anchor))
 
-    def get_latest_day_log(self) -> dict | None:
-        query = "SELECT * FROM day_log ORDER BY id DESC LIMIT 1"
+    def get_latest_day_log(self, columns: tuple[str] | None = None) -> dict | None:
+        for column in columns:
+            if column not in UsagedataService._day_log_columns:
+                raise ValueError(f"Invalid column name: {column}")
+
+        if not columns:
+            columns = UsagedataService._day_log_columns
+
+        query = f"SELECT {', '.join(columns)} FROM day_log ORDER BY id DESC LIMIT 1"
         result = self._db.fetchone(query)
 
         return dict(result) if result else None
@@ -25,6 +40,21 @@ class UsagedataService:
 
         return result[0] if result else 0
 
+    def update_latest_day_log(self, column_values: dict[str, float | int]) -> None:
+        if not column_values:
+            return
+
+        columns = []
+        values = []
+        for column, value in column_values.items():
+            if column not in UsagedataService._day_log_columns:
+                raise ValueError(f"Invalid column name: {column}")
+            columns.append(column + " = ?")
+            values.append(value)
+
+        query = f"UPDATE day_log SET {', '.join(columns)} ORDER BY id DESC LIMIT 1"
+        self._db.execute(query, values)
+
     def remove_oldest_day_log(self) -> None:
-        query = "DELETE FROM day_log WHERE id = (SELECT MIN(id) FROM day_log)"
+        query = "DELETE FROM day_log ORDER BY id ASC LIMIT 1"
         self._db.execute(query)
