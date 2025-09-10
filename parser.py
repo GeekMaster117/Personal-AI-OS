6441a00bc0fd5c55b0ff14285ad4af80dc3ccd4a
GeekMaster117 <saivenkat0306@gmail.com>
2025-09-10 21:24:13 +0530

    Removed model for arguments Seperated getting required and optional methods

--- parser.py
+++ parser.py
@@ -16,10 +16,8 @@ from sklearn.pipeline import Pipeline, make_pipeline
 class Parser:
     commands_path: str = "commands.json"
     commands_hash_path: str = "commands_hash.txt"
-    action_vectorizer_path: str = "action_vectorizer.bin"
-    action_classifier_path: str = "action_classifier.bin"
-    argument_vectorizer_path: str = "argument_vectorizer.bin"
-    argument_classifier_path: str = "argument_classifier.bin"
+    vectorizer_path: str = "vectorizer.bin"
+    classifier_path: str = "classifier.bin"
 
     def __init__(self):
         try:
@@ -37,15 +35,7 @@ class Parser:
             raise RuntimeError(f"Error mapping commands keywords to actions: {e}")
 
         try:
-            action_model = self._load_action_model()
-            self._action_pipeline: Pipeline = action_model[0]
-            self._action_vectorizer: CountVectorizer = action_model[1]
-            self._action_classifier: SGDClassifier = action_model[2]
-
-            argument_model = self._load_argument_model()
-            self._argument_pipeline: Pipeline = argument_model[0]
-            self._argument_vectorizer: CountVectorizer = argument_model[1]
-            self._argument_classifier: SGDClassifier = argument_model[2]
+            self._action_pipeline, self._action_vectorizer, self._action_classifier = self._load_model()
         except Exception as e:
             raise RuntimeError(f"Error loading model: {e}")
 
@@ -93,10 +83,10 @@ class Parser:
             return None
         return joblib.load(classifier_path)
     
-    def _load_action_model(self) -> tuple[Pipeline | CountVectorizer | SGDClassifier]:
+    def _load_model(self) -> tuple[Pipeline, CountVectorizer, SGDClassifier]:
         try:
-            vectorizer = self._load_vectorizer(Parser.action_vectorizer_path)
-            classifier = self._load_classifier(Parser.action_classifier_path)
+            vectorizer = self._load_vectorizer(Parser.vectorizer_path)
+            classifier = self._load_classifier(Parser.classifier_path)
         except:
             vectorizer, classifier = None, None
 
@@ -109,41 +99,13 @@ class Parser:
                 raise RuntimeError(f"Error initialising pipeline: {e}")
             
             try:
-                self._save_vectorizer(vectorizer, Parser.action_vectorizer_path)
-                self._save_classifier(classifier, Parser.action_classifier_path)
+                self._save_vectorizer(vectorizer, Parser.vectorizer_path)
+                self._save_classifier(classifier, Parser.classifier_path)
                 self._save_commands_hash()
             except Exception as e:
                 raise RuntimeError(f"Error saving: {e}")
 
             return pipeline, vectorizer, classifier
-        
-        try:
-            pipeline = make_pipeline(vectorizer, classifier)
-        except Exception as e:
-            raise RuntimeError(f"Error making pipeline: {e}")
-        
-        return pipeline, vectorizer, classifier
-    
-    def _load_argument_model(self) -> tuple[Pipeline | CountVectorizer | SGDClassifier]:
-        try:
-            vectorizer = self._load_vectorizer(Parser.argument_vectorizer_path)
-            classifier = self._load_classifier(Parser.argument_classifier_path)
-        except:
-            vectorizer, classifier = None, None
-
-        if not vectorizer or not classifier or not self._check_commands_hash():
-            try:
-                vectorizer = CountVectorizer()
-                classifier = SGDClassifier(loss="log_loss")
-            except Exception as e:
-                raise RuntimeError(f"Error initialising pipeline: {e}")
-            
-            try:
-                self._save_vectorizer(vectorizer, Parser.argument_vectorizer_path)
-                self._save_classifier(classifier, Parser.argument_classifier_path)
-                self._save_commands_hash()
-            except Exception as e:
-                raise RuntimeError(f"Error saving: {e}")
         
         try:
             pipeline = make_pipeline(vectorizer, classifier)
@@ -164,7 +126,58 @@ class Parser:
 
         return pipeline
     
-    def extract_tokens(self, query: str) -> list[tuple[str | bool]]:
+    def _train(self, keywords: list[str], action: str) -> None:
+        X = self._action_vectorizer.transform([" ".join(keywords)])
+        self._action_classifier.partial_fit(X, [action], classes = list(self.commands.keys()))
+
+        self._save_vectorizer(self._action_vectorizer, Parser.vectorizer_path)
+        self._save_classifier(self._action_classifier, Parser.classifier_path)
+
+    def _handle_options(self, options: list[str], options_message = "Select an option:", select_message = "Enter an answer", key = lambda x: x) -> int:
+        print(options_message)
+        for i, option in enumerate(options, start=1):
+            print(f"{i}. {key(option)}")
+        print(f"{len(options) + 1}. Skip request")
+
+        choice = input(f"{select_message} (1-{len(options) + 1}): ")
+        if choice.isdigit() and 1 <= int(choice) <= len(options):
+            return int(choice) - 1
+        
+        return -1
+    
+    def _get_required_arguments(self, action: str) -> tuple[list[str], Counter]:
+        if "args" not in self.commands[action]:
+            return dict(), Counter()
+        
+        all_arguments: list[dict] = self.commands[action]["args"]
+        
+        arguments, needed = [], Counter()
+        for i in range(len(all_arguments)):
+            if all_arguments[i]["required"]:
+                type = all_arguments[i]["type"]
+
+                arguments.append(type)
+                needed[type] += 1
+        
+        return arguments, needed
+    
+    def _get_optional_arguments(self, action: str) -> tuple[list[str], Counter]:
+        if "args" not in self.commands[action]:
+            return dict(), Counter()
+        
+        all_arguments: list[dict] = self.commands[action]["args"]
+        
+        arguments, needed = [], Counter()
+        for i in range(len(all_arguments)):
+            if not all_arguments[i]["required"]:
+                type = all_arguments[i]["type"]
+
+                arguments.append(type)
+                needed[type] += 1
+        
+        return arguments, needed
+    
+    def _extract_tokens(self, query: str) -> list[tuple[str, bool]]:
         lexer = shlex.shlex(query)
         lexer.whitespace_split = True
 
@@ -181,7 +194,7 @@ class Parser:
         
         return tokens
     
-    def extract_keywords_nkeywords(self, tokens: list[tuple[str | bool]], probability_cutoff: float) -> tuple[list[str]]:
+    def _extract_keywords_nonkeywords(self, tokens: list[tuple[str, bool]], probability_cutoff: float) -> tuple[list[str], list[tuple[str, bool]]]:
         if probability_cutoff < 0 or probability_cutoff > 1:
             raise ValueError(f"Probability cutoff must be in the interval [0, 1], value passed: {probability_cutoff}")
 
@@ -200,22 +213,26 @@ class Parser:
         
         return keywords, non_keywords
     
-    def extract_args(self, tokens: list[tuple[str | bool]]) -> dict:
-        args_types_map = defaultdict(set)
-        for token, quoted in tokens:
+    def _extract_classified_non_keywords(self, non_keywords: list[tuple[str, bool]]) -> tuple[dict, dict]:
+        classified_non_keywords, classified_priority_non_keywords = defaultdict(set), defaultdict(set)
+        for token, quoted in non_keywords:
             if not quoted and token in ENGLISH_STOP_WORDS:
                 continue
 
+            type = "any"
             if token.isdecimal():
-                args_types_map["int"].add(token)
+                type = "int"
             elif token.isalpha():
-                args_types_map["str"].add(token)
+                type = "str"
+            
+            if quoted:
+                classified_priority_non_keywords[type].add(token)
             else:
-                args_types_map["any"].add(token)
+                classified_non_keywords["str"].add(token)
 
-        return args_types_map
+        return classified_non_keywords, classified_priority_non_keywords
 
-    def extract_actions_normalised(self, keywords: list[str]) -> dict:
+    def _extract_actions_normalised(self, keywords: list[str]) -> dict:
         keywords_counter = Counter(keywords)
 
         action_counter = Counter()
@@ -230,7 +247,7 @@ class Parser:
         
         return actions_normalised
 
-    def extract_action_frequency(self, actions_normalised: dict, probability_cutoff: float = 0.85) -> str | None:
+    def _extract_action_frequency(self, actions_normalised: dict, probability_cutoff: float = 0.85) -> str | None:
         if probability_cutoff < 0 or probability_cutoff > 1:
             raise ValueError(f"Probability cutoff must be in the interval [0, 1], value passed: {probability_cutoff}")
 
@@ -243,7 +260,7 @@ class Parser:
         
         return action_normalised[0]
 
-    def extract_actions_classification(self, keywords: list[str], top_actions_count: int) -> list[tuple]:
+    def _extract_actions_classification(self, keywords: list[str], top_actions_count: int) -> list[tuple]:
         probabilities: ndarray = self._action_pipeline.predict_proba([" ".join(keywords)])[0]
 
         classes = [(str(self._action_pipeline.classes_[idx]), float(probability)) for idx, probability in enumerate(probabilities)]
@@ -251,95 +268,54 @@ class Parser:
 
         return top_actions
     
-    def train(self, keywords: list[str], action: str) -> None:
-        X = self._action_vectorizer.transform([" ".join(keywords)])
-        self._action_classifier.partial_fit(X, [action], classes = list(self.commands.keys()))
-
-        self._save_action_model(self._action_vectorizer, self._action_classifier)
-
-    def map_args(self, action: str, args: dict) -> list[str]:
+    def _extract_arguments(self, action: str, classified_non_keywords: dict, classified_priority_non_keywords: dict) -> list[str]:
         try:
-            required_args, optional_args = self.get_args(action)
+            required_arguments, required_needed = self._get_required_arguments(action)
+            optional_arguments, optional_needed = self._get_optional_arguments(action)
         except Exception as e:
             raise RuntimeError(f"Error fetching arguments for action '{action}': {e}")
         
-        if not optional_args and not required_args:
+        if not required_needed or optional_needed:
             return []
         
-        args_count = sum(len(args_set) for args_set in args.values())
-        
-        if args_count < required_args.total():
-            arguments_required = ""
-            for type, needed in required_args.items():
-                arguments_required += f"{type}: {needed}\n"
-            arguments_found = ""
-            for type, args_set in args.items():
-                arguments_found += f"{type}: {len(args_set)}\n"
-            if not arguments_found:
-                arguments_found = "No arguments found"
+        for type, required in required_needed.items():
+            if len(classified_non_keywords.get(type, 0)) + len(classified_priority_non_keywords.get(type, 0)) < required:
+                arguments_required = f"{type} arguments required: {required}"
+                arguments_found = f"{type} arguments found: {classified_non_keywords.get(type, 0)}"
 
-            error_message = (
-                "Found less arguments then required",
-                "Arguments required:",
-                arguments_required,
-                "Arguments found:",
-                arguments_found
-                )
-            raise SyntaxError('\n'.join(error_message))
+                error_message = (
+                    "Found less arguments then required",
+                    arguments_required,
+                    arguments_found
+                    )
+                raise SyntaxError('\n'.join(error_message))
         
         return []
 
-    def handle_options(self, options: list[str], options_message = "Select an option:", select_message = "Enter an answer", key = lambda x: x) -> int:
-        print(options_message)
-        for i, option in enumerate(options, start=1):
-            print(f"{i}. {key(option)}")
-        print(f"{len(options) + 1}. Skip request")
-
-        choice = input(f"{select_message} (1-{len(options) + 1}): ")
-        if choice.isdigit() and 1 <= int(choice) <= len(options):
-            return int(choice) - 1
-        
-        return -1
-    
-    def get_args(self, action: str) -> tuple[Counter]:
-        if "args" not in self.commands[action]:
-            return Counter()
-        
-        required_args, optional_args = Counter(), Counter()
-        for i in range(len(self.commands[action]["args"])):
-            type = self.commands[action]["args"][i]["type"]
-
-            if self.commands[action]["args"][i]["required"]:
-                required_args[type] += 1
-            else:
-                optional_args[type] += 1
-
-        return required_args, optional_args
-
-    def get_action_args(self, query: str, probability_cutoff: float = 0.85) -> tuple[str | list[str]]:
+    def extract_action_arguments(self, query: str, probability_cutoff: float = 0.85) -> tuple[str | list[str]]:
         try:
-            tokens: list[tuple[str | bool]] = self.extract_tokens(query)
+            tokens: list[tuple[str | bool]] = self._extract_tokens(query)
         except Exception as e:
             raise SyntaxError(f"Syntax Error: {e}")
 
         try:
-            keywords, non_keywords = self.extract_keywords_nkeywords(tokens, probability_cutoff)
+            keywords, non_keywords = self._extract_keywords_nonkeywords(tokens, probability_cutoff)
         except Exception as e:
             raise RuntimeError(f"Error extracting keywords: {e}")
         
         if not keywords:
             raise SyntaxError("No keywords found")
         
-        actions_normalised: dict = self.extract_actions_normalised(keywords)
+        actions_normalised: dict = self._extract_actions_normalised(keywords)
         
         try:
-            action = self.extract_action_frequency(actions_normalised, probability_cutoff)
+            action = self._extract_action_frequency(actions_normalised, probability_cutoff)
         except Exception as e:
             raise RuntimeError(f"Error extracting action: {e}")
         
         if not action:
             try:
-                actions = self.extract_actions_classification(keywords, 5)
+                actions = self._extract_actions_classification(keywords, 5)
             except Exception as e:
                 raise RuntimeError(f"Error extracting top actions: {e}")
             
@@ -347,7 +323,7 @@ class Parser:
                 action = actions[0][0]
             else:
                 try:
-                    answer = self.handle_options(actions, options_message = "What do you want to do?", key = lambda action: self.commands[action[0]]["description"])
+                    answer = self._handle_options(actions, options_message = "What do you want to do?", key = lambda action: self.commands[action[0]]["description"])
                     print("-----------------------------")
                 except Exception as e:
                     raise RuntimeError(f"Error fetching answer: {e}")
@@ -355,17 +331,17 @@ class Parser:
                 if answer == -1:
                     return "", []
 
-                self.train(keywords, actions[answer][0])
+                self._train(keywords, actions[answer][0])
                 action = actions[answer][0]
 
-        args: dict = self.extract_args(non_keywords)
+        classified_non_keywords, classified_priority_non_keywords = self._extract_classified_non_keywords(non_keywords)
 
         try:
-            mapped_args: list[str] = self.map_args(action, args)
+            arguments: list[str] = self._extract_arguments(action, classified_non_keywords, classified_priority_non_keywords)
         except Exception as e:
             raise SyntaxError(f"Error mapping arguments: {e}")
         
-        return action, mapped_args
+        return action, arguments
     
     def execute_action(self, action : str, args: list[str]) -> None:
         if self.commands[action]["warning"] == True:
@@ -389,7 +365,7 @@ print("-----------------------------")
 action: str | None = None
 while True:
     try:
-        action, args = parser.get_action_args(query)
+        action, args = parser.extract_action_arguments(query)
 
         if action:
             break
