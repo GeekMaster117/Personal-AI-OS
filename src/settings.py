88be16885680b20e278e68a0ea1afce6cec512bf
GeekMaster117 <saivenkat0306@gmail.com>
2025-09-28 21:40:42 +0530

    used os join to combine paths

--- src/settings.py
+++ src/settings.py
@@ -1,3 +1,5 @@
+import os
+
 from datetime import timedelta
 
 tick: timedelta = timedelta(seconds=30)
@@ -5,26 +7,26 @@ time_threshold: timedelta = timedelta(minutes=3)
 
 usagedata_dir: str = "data"
 sql_dir: str = "sql"
-schema_dir: str = sql_dir + "/schema.sql"
-fetch_all_apps_titles_dir: str = sql_dir + "/fetch_all_apps_titles.sql"
+schema_dir: str = os.path.join(sql_dir, "schema.sql")
+fetch_all_apps_titles_dir: str = os.path.join(sql_dir, "fetch_all_apps_titles.sql")
 
 library_dir: str = "bin"
 
 cache_dir: str = "cache"
 model_dir: str = "models/Phi-3-mini-4k-instruct-q4.gguf"
 
-parser_dir: str = library_dir + "/parser"
-commands_dir: str = parser_dir + "/commands.bin"
-keyword_action_map_dir: str = parser_dir + "/keyword_action_map.bin"
-action_pipeline_dir: str = parser_dir + "/action_pipeline.bin"
+parser_dir: str = os.path.join(library_dir, "parser")
+commands_dir: str = os.path.join(parser_dir, "commands.bin")
+keyword_action_map_dir: str = os.path.join(parser_dir, "keyword_action_map.bin")
+action_pipeline_dir: str = os.path.join(parser_dir, "action_pipeline.bin")
 
-keyword_argument_maps_dir: str = parser_dir + "/keyword argument maps"
+keyword_argument_maps_dir: str = os.path.join(parser_dir, "keyword argument maps")
 def keyword_argument_map_dir(action: str) -> str:
-    return keyword_argument_maps_dir + "/" + action + "/keyword_argument_map.bin"
+    return os.path.join(keyword_argument_maps_dir, action, "keyword_argument_map.bin")
 
-argument_pipelines_dir: str = parser_dir + "/argument pipelines"
+argument_pipelines_dir: str = os.path.join(parser_dir, "argument pipelines")
 def argument_pipeline_dir(action: str) -> str:
-    return argument_pipelines_dir + "/" + action + "/argument_pipeline.bin"
+    return os.path.join(argument_pipelines_dir, action, "argument_pipeline.bin")
 
 total_model_layers: int = 32
 model_window_size: int = 2048
