6743557a33ffe235fd64de0bee4fa3614d100bd2
GeekMaster117 <saivenkat0306@gmail.com>
2025-09-29 23:26:44 +0530

    no longer relies on psutil to get app name

--- src/Include/app_monitor.py
+++ src/Include/app_monitor.py
@@ -1,5 +1,4 @@
 import pywinctl
-import psutil
 
 import Include.filter.app_title_blacklist as blacklist
 
@@ -22,57 +21,39 @@ def get_all_apps() -> dict[str, set[str]]:
     result: dict[str, set[str]] = {}
 
     for win in pywinctl.getAllWindows():
-        try:
-            pid: int | None = win.getPID()
-            proc: psutil.Process | None = psutil.Process(pid)
-            if not proc:
-                continue
+        name: str = win.getAppName()
 
-            name: str = proc.name()
-
-            # Skip blacklisted apps
-            if is_app_blacklisted(name):
-                continue
-
-            title: str = win.title.strip()
-            
-            # Skip hidden or empty
-            if not title or not win.isVisible:
-                continue
-
-            # Skip blacklisted windows
-            if is_title_blacklisted(title, name):
-                continue
-
-            # Group all window titles under one app
-            if name not in result:
-                result[name] = set()
-            result[name].add(title)
-
-        except (psutil.NoSuchProcess, psutil.AccessDenied):
+        # Skip blacklisted apps
+        if is_app_blacklisted(name):
             continue
+
+        title: str = win.title.strip()
+        
+        # Skip hidden or empty
+        if not title or not win.isVisible:
+            continue
+
+        # Skip blacklisted windows
+        if is_title_blacklisted(title, name):
+            continue
+
+        # Group all window titles under one app
+        if name not in result:
+            result[name] = set()
+        result[name].add(title)
 
     return result
 
 def get_active_app_title() -> tuple[str, str] | None:
-    try:
-        active_window = pywinctl.getActiveWindow()
-        if not active_window:
-            return None, None
-
-        pid: int | None = active_window.getPID()
-        proc: psutil.Process | None = psutil.Process(pid)
-        if not proc:
-            return None, None
-
-        name: str = proc.name()
-        title: str = active_window.title.strip()
-
-        # Skip blacklisted apps
-        if is_app_blacklisted(name) or is_title_blacklisted(title, name):
-            return None, None
-
-        return name, title
-
-    except (psutil.NoSuchProcess, psutil.AccessDenied):
+    active_window = pywinctl.getActiveWindow()
+    if not active_window:
         return None, None
+
+    name: str = active_window.getAppName()
+    title: str = active_window.title.strip()
+
+    # Skip blacklisted apps
+    if is_app_blacklisted(name) or is_title_blacklisted(title, name):
+        return None, None
+
+    return name, title
