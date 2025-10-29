5558c29bb068a28d2024a97533d605eaf3607483
GeekMaster117 <saivenkat0306@gmail.com>
2025-10-29 22:28:30 +0530

    wrapped extracting package logic in a function

--- dev/install_packages.py
+++ dev/install_packages.py
@@ -28,17 +28,35 @@ def install_package(env: list[str], package: list[str], retries: int = 3):
     print(f"Failed to install {package} after {retries} attempts.\n")
     return False
 
-if __name__ == "__main__":
-    with open(os.path.join("dev", "requirements", "python_requirements.txt"), "r") as file:
+def extract_packages(requirements_dir: str) -> list[tuple[list, list]]:
+    if not os.path.exists(requirements_dir):
+        raise FileNotFoundError(f'requirements file at "{requirements_dir}" not found')
+    if os.path.isdir(requirements_dir):
+        raise FileNotFoundError(f'requirements path "{requirements_dir}" cannot be a folder')
+
+    with open(requirements_dir, "r") as file:
+        env_packages = []
+
         for line in file:
             env_package = line.strip().split('@')
             if not env_package:
                 continue
-            
+
             if len(env_package) == 1:
-                install_package([], env_package)
+                env, package = [], env_package[0].split(';')
             else:
                 env, package = env_package[0].split(';'), env_package[1].split(';')
-                if not package:
-                    continue
-                install_package(env, package)
+            
+            if not package:
+                continue
+            
+            env_packages.append((env, package))
+
+        return env_packages
+
+if __name__ == "__main__":
+    requirements_dir = os.path.join("dev", "requirements", "python_requirements.txt")
+    env_packages = extract_packages(requirements_dir)
+
+    for env, package in env_packages:
+        install_package(env, package)
